19483c71b2135a8ce621f73d19bf08b2b3f6a988
REVNBleach <109180390+REVNBleach@users.noreply.github.com>
2022-08-15 21:35:58 +0300

    Create read_input_functions.h

---
+++ поисковый сервер/read_input_functions.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+string ReadLine();
+
+int ReadLineWithNumber();
+
+vector<string> SplitIntoWords(const string& text);
+
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+template <typename StringContainer>
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    set<string> non_empty_strings;
+    for (const string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
