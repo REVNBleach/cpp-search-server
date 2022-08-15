de367fb3390fe6bb9c31b582d5e49bc2f832d1dd
REVNBleach <109180390+REVNBleach@users.noreply.github.com>
2022-08-15 21:33:12 +0300

    Create document.h

---
+++ поисковый сервер/document.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+
+using namespace std;
+
+
+struct Document {
+    Document();
+
+    Document(int id, double relevance, int rating);
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
