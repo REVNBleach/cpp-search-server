b40d89c2cfc605f1fd583112cfa729856c0fc824
REVNBleach <109180390+REVNBleach@users.noreply.github.com>
2022-08-16 15:35:43 +0300

    Update request_queue.h

--- поисковый сервер/request_queue.h
+++ поисковый сервер/request_queue.h
@@ -12,26 +12,48 @@
 #include <vector>
 #include <deque>
 
-using namespace std;
-
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
     // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+        QueryResult qr;
+        if(requests_.size()==min_in_day_)
+        {
+            requests_.pop_front();
+            if(requests_.front().EmptyResult==true)
+            {
+                num_of_empty_res--;
+            }
+        }
+        std::vector<Document> res = search_server_.FindTopDocuments(raw_query, document_predicate);
+        if(res.empty())
+        {
+            qr.results=res;
+            qr.EmptyResult=true;
+            num_of_empty_res++;
+        }
+        else
+        {
+            qr.results=res;
+            qr.EmptyResult=false;
+        }
+        requests_.push_back(qr);
+        return res;
+    }
 
-    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
-    vector<Document> AddFindRequest(const string& raw_query);
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
 
     int GetNoResultRequests() const;
 private:
     struct QueryResult {
-        vector<Document> results;
+        std::vector<Document> results;
         bool EmptyResult;
     };
-    deque<QueryResult> requests_;
+    std::deque<QueryResult> requests_;
     const static int min_in_day_ = 1440;
     int num_of_empty_res;
     const SearchServer& search_server_;
