9b3f71bbe986e9937bf68b9f0ca4f0d906ed9848
REVNBleach <109180390+REVNBleach@users.noreply.github.com>
2022-08-16 15:39:16 +0300

    Update string_processing.h

--- поисковый сервер/string_processing.h
+++ поисковый сервер/string_processing.h
@@ -4,15 +4,13 @@
 #include "document.h"
 #include <string>
 
-using namespace std;
-
 void PrintDocument(const Document& document);
 
-void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status);
+void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status);
 
-void AddDocument(SearchServer& search_server, int document_id, const string& document, DocumentStatus status,
-                 const vector<int>& ratings);
+void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status,
+                 const std::vector<int>& ratings);
 
-void FindTopDocuments(const SearchServer& search_server, const string& raw_query);
+void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query);
 
-void MatchDocuments(const SearchServer& search_server, const string& query);
+void MatchDocuments(const SearchServer& search_server, const std::string& query);
