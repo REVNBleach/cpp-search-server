6c3fbb54b31fbccbd0e0bf2a2cb5b63e6c37c46d
REVNBleach <109180390+REVNBleach@users.noreply.github.com>
2022-08-17 09:33:31 +0300

    Update request_queue.h

--- поисковый сервер/request_queue.h
+++ поисковый сервер/request_queue.h
@@ -17,7 +17,26 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
     // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate); 
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+
+    int GetNoResultRequests() const;
+private:
+    struct QueryResult {
+        std::vector<Document> results;
+        bool EmptyResult;
+    };
+    std::deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    int num_of_empty_res;
+    const SearchServer& search_server_;
+}; 
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
         QueryResult qr;
         if(requests_.size()==min_in_day_)
         {
@@ -42,19 +61,3 @@ public:
         requests_.push_back(qr);
         return res;
     }
-
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
-
-    std::vector<Document> AddFindRequest(const std::string& raw_query);
-
-    int GetNoResultRequests() const;
-private:
-    struct QueryResult {
-        std::vector<Document> results;
-        bool EmptyResult;
-    };
-    std::deque<QueryResult> requests_;
-    const static int min_in_day_ = 1440;
-    int num_of_empty_res;
-    const SearchServer& search_server_;
-}; 
